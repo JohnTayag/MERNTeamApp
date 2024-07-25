97472f0e362fc39dbee2f15dca6c9b577b459482
JohnTayag <94370704+JohnTayag@users.noreply.github.com>
2024-07-26 00:21:40 +0100

    Implemented create a match form for admin

--- mern/client/src/components/Matches.jsx
+++ mern/client/src/components/Matches.jsx
@@ -4,7 +4,9 @@ import { useParams, useNavigate } from "react-router-dom";
 export default function Record() {
   const [form, setForm] = useState({
     name: "",
-    position: "",
+    location: "",
+    description: "",
+    date: "",
     level: "",
   });
   const [isNew, setIsNew] = useState(true);
@@ -17,20 +19,20 @@ export default function Record() {
       if(!id) return;
       setIsNew(false);
       const response = await fetch(
-        `http://localhost:5050/record/${params.id.toString()}`
+        `http://localhost:5050/match/${params.id.toString()}`
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
         console.error(message);
         return;
       }
-      const record = await response.json();
-      if (!record) {
-        console.warn(`Record with id ${id} not found`);
+      const match = await response.json();
+      if (!match) {
+        console.warn(`Match with id ${id} not found`);
         navigate("/");
         return;
       }
-      setForm(record);
+      setForm(match);
     }
     fetchData();
     return;
@@ -50,8 +52,8 @@ export default function Record() {
     try {
       let response;
       if (isNew) {
-        // if we are adding a new record we will POST to /record.
-        response = await fetch("http://localhost:5050/record", {
+        // if we are adding a new match we will POST to /match.
+        response = await fetch("http://localhost:5050/match", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -59,8 +61,8 @@ export default function Record() {
           body: JSON.stringify(person),
         });
       } else {
-        // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://localhost:5050/record/${params.id}`, {
+        // if we are updating a match we will PATCH to /match/:id.
+        response = await fetch(`http://localhost:5050/match/${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
@@ -75,7 +77,7 @@ export default function Record() {
     } catch (error) {
       console.error('A problem occurred with your fetch operation: ', error);
     } finally {
-      setForm({ name: "", position: "", level: "" });
+      setForm({ name: "", date: "", level: "" });
       navigate("/");
     }
   }
@@ -83,7 +85,7 @@ export default function Record() {
   // This following section will display the form that takes the input from the user.
   return (
     <>
-      <h3 className="text-lg font-semibold p-4">Create/Update Employee Record</h3>
+      <h3 className="text-lg font-semibold p-4">Create Match</h3>
       <form
         onSubmit={onSubmit}
         className="border rounded-lg overflow-hidden p-4"
@@ -91,7 +93,7 @@ export default function Record() {
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2">
           <div>
             <h2 className="text-base font-semibold leading-7 text-slate-900">
-              Employee Info
+              Match Info
             </h2>
             <p className="mt-1 text-sm leading-6 text-slate-600">
               This information will be displayed publicly so be careful what you
@@ -114,31 +116,76 @@ export default function Record() {
                     name="name"
                     id="name"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="First Last"
+                    placeholder="Match Name"
                     value={form.name}
                     onChange={(e) => updateForm({ name: e.target.value })}
                   />
                 </div>
               </div>
             </div>
+
             <div className="sm:col-span-4">
               <label
-                htmlFor="position"
+                htmlFor="location"
                 className="block text-sm font-medium leading-6 text-slate-900"
               >
-                Position
+                Location
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
                     type="text"
-                    name="position"
-                    id="position"
+                    name="location"
+                    id="location"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="Developer Advocate"
-                    value={form.position}
-                    onChange={(e) => updateForm({ position: e.target.value })}
+                    placeholder="location Name"
+                    value={form.location}
+                    onChange={(e) => updateForm({ location: e.target.value })}
                   />
+                </div>
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="description"
+                className="block text-sm font-medium leading-6 text-slate-900"
+              >
+                description
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="description"
+                    id="description"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="description"
+                    value={form.description}
+                    onChange={(e) => updateForm({ description: e.target.value })}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="date"
+                className="block text-sm font-medium leading-6 text-slate-900"
+              >
+                Date
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+  type="date"
+  name="position"
+  id="position"
+  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+  placeholder="Select a date"
+  value={form.position}
+  onChange={(e) => updateForm({ position: e.target.value })}
+/>
                 </div>
               </div>
             </div>
@@ -147,21 +194,7 @@ export default function Record() {
                 <legend className="sr-only">Position Options</legend>
                 <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
                   <div className="flex items-center">
-                    <input
-                      id="positionIntern"
-                      name="positionOptions"
-                      type="radio"
-                      value="Intern"
-                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
-                      checked={form.level === "Intern"}
-                      onChange={(e) => updateForm({ level: e.target.value })}
-                    />
-                    <label
-                      htmlFor="positionIntern"
-                      className="ml-3 block text-sm font-medium leading-6 text-slate-900 mr-4"
-                    >
-                      Intern
-                    </label>
+                
                     <input
                       id="positionJunior"
                       name="positionOptions"
@@ -200,7 +233,7 @@ export default function Record() {
         </div>
         <input
           type="submit"
-          value="Save Employee Record"
+          value="Save Match Record"
           className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4"
         />
       </form>
